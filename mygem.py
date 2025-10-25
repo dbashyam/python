aaec4a76494e54f3a44072efdcc7b381b6235792
Dinesh Babu Bashyam <dineshbabub@gmail.com>
2025-10-25 13:14:44 +0800

    Integrate Streamlit for user input and response display

--- mygem.py
+++ mygem.py
@@ -1,9 +1,11 @@
 from google import genai
+import streamlit as st
 
 client = genai.Client(api_key="AIzaSyBcI3rMgFb2ShRbCKiS2ypcPeF6jbYoiXs")
-
-response = client.models.generate_content(
-    model="gemma-3-27b-it",
-    contents="How does AI work?"
-)
-print(response. Text)
+prompt = st.chat_input("enter your message:")
+if prompt is not None:
+    response = client.models.generate_content(
+        model="gemma-3-27b-it",
+        contents=prompt
+    )
+    st.write(response. Text)
